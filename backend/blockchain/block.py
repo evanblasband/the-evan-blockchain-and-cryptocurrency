979b3e1556ce1546b3ea991c411d3307a53cd06d
Evan Blasband <eblasband@gmail.com>
2022-03-03 18:20:04 -0800

    renaming colliding hash_ name and making global genesis data for testing

--- backend/blockchain/block.py
+++ backend/blockchain/block.py
@@ -2,6 +2,13 @@ import time
 
 from backend.utils.crypto_hash import crypto_hash
 
+GENESIS_DATA = {
+    "timestamp": 1,
+    "last_hash": "genesis_last_hash",
+    "hash_": "genesis_hash",
+    "data": [],
+}
+
 
 class Block:
     """
@@ -14,7 +21,7 @@ class Block:
         """Constructor for Block"""
         self.timestamp = timestamp
         self.last_hash = last_hash
-        self.hash = hash_
+        self.hash_ = hash_
         self.data = data
 
     def __repr__(self):
@@ -22,7 +29,7 @@ class Block:
             f"Block("
             f"timestamp: {self.timestamp}, "
             f"last_hash: {self.last_hash}, "
-            f"hash: {self.hash}, "
+            f"hash_: {self.hash_}, "
             f"data: {self.data}, "
             f")"
         )
@@ -32,12 +39,12 @@ class Block:
         """
         Creates a block given the last block and the given data
 
-        :param last_block: the last Block so that we can get its hash value
+        :param last_block: the last Block so that we can get its hash_ value
         :param data: whatever data is to be put in this block
         :return: a new block to be added to the chain
         """
         timestamp = time.time_ns()
-        last_hash = last_block.hash
+        last_hash = last_block.hash_
         hash_ = crypto_hash(timestamp, last_hash, data)
 
         return Block(timestamp=timestamp, last_hash=last_hash, hash_=hash_, data=data)
@@ -48,9 +55,14 @@ class Block:
         Creates the first block to start the blockchain
         :return: an initial block to start the chain
         """
-        return Block(
-            timestamp=1, last_hash="genesis_last_hash", hash_="genesis_hash", data=[]
-        )
+        # return Block(
+        #     timestamp=GENESIS_DATA['timestamp'],
+        #     last_hash=GENESIS_DATA['last_hash'],
+        #     hash_=GENESIS_DATA['genesis_hash'],
+        #     data=GENESIS_DATA['data']
+        # )
+        # Creates the same as above
+        return Block(**GENESIS_DATA)
 
 
 def main():
