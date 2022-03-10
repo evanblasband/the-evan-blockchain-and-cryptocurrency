452cf0f42eb9275861d6dfe6fd3eb0c41cf49804
Evan Blasband <eblasband@gmail.com>
2022-03-09 17:18:33 -0800

    Tests for adjust difficulty

--- backend/test/blockchain/test_block.py
+++ backend/test/blockchain/test_block.py
@@ -1,4 +1,7 @@
+import time
+
 from backend.blockchain.block import GENESIS_DATA, Block
+from backend.config import MINE_RATE, SECONDS
 
 
 def test_mine_block():
@@ -28,3 +31,48 @@ def test_genesis():
     assert isinstance(genesis, Block)
     for key, value in GENESIS_DATA.items():
         assert getattr(genesis, key) == value
+
+
+def test_adjust_difficulty_quickly_mined():
+    """
+    tests the adjust_difficulty function when a block is mined too fast.
+    should increase the difficulty by 1
+    :return:
+    """
+    last_block = Block.mine_block(last_block=Block.genesis(), data="test")
+    mined_block = Block.mine_block(last_block=last_block, data="test2")
+
+    assert mined_block.difficulty == last_block.difficulty + 1
+
+
+def test_adjust_difficulty_slowly_mined():
+    """
+    tests the adjust_difficulty function when a block is mined too slow.
+    should decrease the difficulty by 1
+    :return:
+    """
+    last_block = Block.mine_block(last_block=Block.genesis(), data="test")
+    time.sleep(MINE_RATE / SECONDS)  # MINE_RATE is 4B ns so we need to
+    # divide be seconds
+    mined_block = Block.mine_block(last_block=last_block, data="test2")
+
+    assert mined_block.difficulty == last_block.difficulty - 1
+
+
+def test_adjust_difficulty_default():
+    """
+    making sure the adjust block function can never set a negative difficulty
+    :return:
+    """
+    last_block = Block(
+        timestamp=time.time_ns(),
+        last_hash="test_last_hash",
+        data="test_data",
+        hash_="test_hash",
+        difficulty=1,
+        nonce=0,
+    )
+    time.sleep(MINE_RATE / SECONDS)
+    mined_block = Block.mine_block(last_block=last_block, data="test2")
+
+    assert mined_block.difficulty == 1
