460e702a10d26582ba244f9f93c7da82e33e6c9c
Evan Blasband <eblasband@gmail.com>
2022-03-11 17:23:48 -0800

    endpoint for getting blockchain data with json

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,17 +1,33 @@
-from flask import Flask
+import flask
+from flask import Flask, jsonify
+
+from backend.blockchain.blockchain import Blockchain
 
 app = Flask(__name__)
+blockchain = Blockchain()
+
+for i in range(3):
+    blockchain.add_block(data=i)
 
 
 @app.route("/")
-def default():
+def route_default() -> str:
+    """
+    the homepage for the blockchain
+    :return:
+    """
     return "Welcome to the EBBlockchain"
 
 
-# @app.route("/blockchain")
-# def get_blockchain():
-#
-#
+@app.route("/blockchain")
+def route_get_blockchain() -> flask.Response:
+    """
+    route for getting the blockchain data
+    :return: returns the blockchain list as json
+    """
+    return jsonify(blockchain.to_json())
+
+
 # @app.route("/mine_block")
 # def add_block():
 
