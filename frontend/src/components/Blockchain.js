4222d0bfe08d2baa4669afad0488b473bbdd62cb
Evan Blasband <eblasband@gmail.com>
2022-04-11 20:16:48 -0700

    adding pagination to viewing blockchain

--- frontend/src/components/Blockchain.js
+++ frontend/src/components/Blockchain.js
@@ -1,15 +1,32 @@
 import React, { useState, useEffect } from "react";
+import { Button } from "react-bootstrap";
 import { API_BASE_URL } from "../Config";
 import Block from "./Block";
 
+const PAGE_RANGE = 3;
+
 function Blockchain() {
   const [blockchain, setBlockchain] = useState([]);
+  const [blockchainLength, setBlockchainLenth] = useState(0);
 
-  useEffect(() => {
-    fetch(`${API_BASE_URL}/blockchain`)
+  const fetchBlockchainPage = ({ start, end }) => {
+    fetch(`${API_BASE_URL}/blockchain/range?start=${start}&end=${end}`)
       .then((response) => response.json())
       .then((json) => setBlockchain(json));
+  };
+
+  useEffect(() => {
+    fetchBlockchainPage({ start: 0, end: PAGE_RANGE });
+
+    fetch(`${API_BASE_URL}/blockchain/length`)
+      .then((response) => response.json())
+      .then((json) => setBlockchainLenth(json));
   }, []);
+
+  const buttonNumbers = [];
+  for (let i = 0; i < Math.ceil(blockchainLength / PAGE_RANGE); i++) {
+    buttonNumbers.push(i);
+  }
 
   return (
     <div className="Blockchain">
@@ -18,6 +35,23 @@ function Blockchain() {
         {blockchain.map((block) => (
           <Block key={block.hash_} block={block} />
         ))}
+      </div>
+      <div>
+        {buttonNumbers.map((number) => {
+          const start = number * PAGE_RANGE;
+          const end = (number + 1) * PAGE_RANGE;
+          return (
+            <span key={number}>
+              <Button
+                size="sm"
+                variant="danger"
+                onClick={() => fetchBlockchainPage({ start, end })}
+              >
+                {number + 1}
+              </Button>{" "}
+            </span>
+          );
+        })}
       </div>
     </div>
   );
