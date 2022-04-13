90dd1fa537eb053dda95d292ce43dcfb8f8ce085
Evan Blasband <eblasband@gmail.com>
2022-04-13 16:13:42 -0700

    Button to mine block

--- frontend/src/components/TransactionPool.js
+++ frontend/src/components/TransactionPool.js
@@ -2,10 +2,13 @@ import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import Transaction from "./Transaction";
 import { API_BASE_URL, SECONDS_JS } from "../Config";
+import { Button } from "react-bootstrap";
+import { useNavigate } from "react-router";
 
 const POLL_INTERVAL = 10 * SECONDS_JS;
 
 function TransactionPool() {
+  let navigate = useNavigate();
   const [transactions, setTransactions] = useState([]);
 
   const fetchTransactions = () =>
@@ -14,6 +17,22 @@ function TransactionPool() {
       .then((json) => {
         console.log("transactions json", json);
         setTransactions(json);
+      });
+
+  const mineTransaction = () =>
+    fetch(`${API_BASE_URL}/blockchain/mine`)
+      .then((response) => {
+        console.log(response.status);
+        if (response.status === 200) {
+          return response.json();
+        } else {
+          alert("Error: ", response.body);
+        }
+      })
+      .then((json) => {
+        console.log(json);
+        alert("Successfully mined the block");
+        navigate("/blockchain");
       });
 
   useEffect(() => {
@@ -35,6 +54,10 @@ function TransactionPool() {
           </div>
         ))}
       </div>
+      <hr />
+      <Button variant="danger" onClick={mineTransaction}>
+        Mine a block of these transactions
+      </Button>
     </div>
   );
 }
